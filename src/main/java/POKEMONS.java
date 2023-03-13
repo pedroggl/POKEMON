6275ffdd97daa55712a17f14a1d0fe36e4142ab0
pedroggl <gallardolozanopedrogabriel@gmail.com>
2023-03-13 17:05:40 +0100

    Probando git push desde el instituto

--- src/main/java/POKEMONS.java
+++ src/main/java/POKEMONS.java
@@ -4,7 +4,7 @@
  * @author galla
  */
 public class POKEMONS {
-    
+    //probando un git push desde el ordenador de clase
     public static void main(String[] args) {
         //Creo entrenador con un starter
 //        Entrenador pedro = new Entrenador("Pedro", new Pokemon(0, 0, 0, 0, 0, 0));
